6257f714f39cab6c49518729aedab409ba6bad8c
Wilhelm Lövgren Olofsson <wille@MacBook-Pro.local>
2024-12-11 15:02:05 +0100

    Completed the backend with endpoints with room for adjustments for a simple frontend to complete the practical part of the degree project.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,14 +1,19 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const privateKey = vars.get("PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
     solidity: "0.8.28",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
     networks: {
-        ganache: {
-            url: "http://127.0.0.1:7545",
-            accounts: [privateKey],
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY],
         },
     },
 };
